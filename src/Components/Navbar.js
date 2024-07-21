123fcc4b39a3c102202d52f6abbc81b6fc83f2c7
Tashi <tashishyangba23@gmail.com>
2024-07-21 14:27:46 -0400

    first commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div>
+      <ul>
+        <li>About me</li>
+        <li>Projects</li>
+        <li>Contact me</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar;
